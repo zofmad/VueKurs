4f34f6520a06ecdd79123134e7f1ca89b4a8360c
Madej Zofia <Zofia.Madej@external.t-mobile.pl>
2022-06-02 14:47:00 +0200

    1.2.exercise

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+
+<template>
+<h2>{{title}}</h2>
+<MyCounter/>
+<MyCounter/>
+<MyCounter/>
+<RandomNumber/>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+<script>
+import MyCounter from './components/Counter'
+import RandomNumber from './components/RandomNumber'
+
+export default {
+  name: 'App',
+  data: () => ({ title: 'Title' }),
+  components: {
+    MyCounter, RandomNumber
+  }
+}
+
+</script>
---
+++ src/components/RandomNumber.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+   <h1>Randomize</h1>
+    <span>{{ number }}</span>
+    <button @click="randomize">Randomize</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: () => ({
+    number: 0
+  }),
+  methods: {
+    randomize () {
+    // funkcja Math.random() zwraca liczby od 0(włącznie) do 1(wyłącznie), zatem trzeba dodac 1, zeby otrzymać liczby od 1 do 100
+      this.number = Math.round(Math.random() * 100, 0) + 1
+    }
+  }
+}
+</script>
