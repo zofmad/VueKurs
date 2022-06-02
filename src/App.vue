1ae4bd4b168bf7b6853bb9820cb10f35b5269202
Madej Zofia <Zofia.Madej@external.t-mobile.pl>
2022-06-02 15:41:55 +0200

    app.vue script before style

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,6 @@
 <RandomNumber/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
 <script>
 import MyCounter from './components/Counter'
 import RandomNumber from './components/RandomNumber'
@@ -30,3 +20,14 @@ export default {
 }
 
 </script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
