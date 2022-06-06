cdd0f21747a16f169d7fc3db9411124c7b3cb671
Madej Zofia <Zofia.Madej@external.t-mobile.pl>
2022-06-06 15:28:32 +0200

    1.4 task

--- 1.3/src/App.vue
+++ 1.3/src/App.vue
@@ -4,29 +4,22 @@
     <header>
       <h1>Books<span>.app</span></h1>
     </header>
-<books-list :myBooks="books" @remove="removeBook" />
+    <books-list :myBooks="books" @remove="removeBook" />
     <!-- zadanie 1 -->
     <div>
       <p>{{booksAmount}}</p>
     </div>
     <!-- add book form -->
-
-    <form>
-      <label>
-        Title:
-        <input v-model="newBook.title" type="text" name="title">
-      </label>
-      <label>
-        price:
-        <input v-model="newBook.price" type="number" name="price">
-      </label>
-      <button @click.prevent="addBook">Add book</button>
-    </form>
-
+    <books-length-msg :nbOfBooks="books.length"/>
+    <books-form @addBook="addBook"/>
+    <books-summary :nbOfBooks="books.length" :totalPrice="booksPrice"/>
   </div>
 </template>
 <script>
 import BooksList from './components/BooksList'
+import BooksLengthMsg from './components/BooksLengthMsg'
+import BooksForm from './components/BooksForm'
+import BooksSummary from './components/BooksSummary'
 
 export default {
   name: 'App',
@@ -38,32 +31,28 @@ export default {
     {
       title: 'b',
       price: 15
-    }],
-    newBook: { title: '', price: 0 }
+    }]
   }),
   methods: {
-    addBook () {
-      this.books.push(this.newBook)
-      // zadanie 2
-      this.newBook = { title: '', price: 0 }
+    addBook (newBook) {
+      this.books.push(newBook)
     },
     removeBook (i) { this.books.splice(i, 1) }
   },
-  computed: {
-    booksAmount () {
-      if (this.books.length > 5) {
-        return this.books.length + 'books'
-      } else if (this.books.length > 1) {
-        return 'Not too many of them…'
-      } else if (this.books.length === 1) {
-        return 'One single book!'
-      } else {
-        return 'Go get some books!'
-      }
-    }
-  },
   components: {
-    BooksList
+    BooksList,
+    BooksLengthMsg,
+    BooksForm,
+    BooksSummary
+  },
+  computed: {
+    booksPrice () {
+      let totalPrice = 0
+      for (let i = 0; i < this.books.length; i++) {
+        totalPrice += parseInt(this.books[i].price)
+      }
+      return totalPrice
+    }
   }
 }
 </script>
